210e1d8dc1d734b11d2eca6359190d1c988624eb
froncheek <jerwin.ilagan@gmail.com>
2012-09-11 02:31:43 +0800

    test

--- leya.js
+++ leya.js
@@ -10,16 +10,25 @@
     var win = window,
         doc = win.document;
 
-    var leya = {};
+    var leya = {},
+        alias;
 
     leya.__proto__ = {
-        create:function (ns, conf) {
+        create: function (ns, conf) {
             var o = this.ns(ns);
 
             if (o) {
+                var ext = conf.extend;
 
+                if(this.get(ext)) {
+
+                }
+
+                this.reg('model', o);
             }
-        }, each:function (o, fncbk, scp) {
+            return false;
+        }
+        ,each: function (o, fncbk, scp) {
             var len = o.length,
                 isO = len == undefined,
                 v, k = 0;
@@ -41,7 +50,8 @@
                     }
                 }
             }
-        }, extend:function (fr, to) {
+        }
+        ,extend: function (fr, to) {
             var o = to,
                 v;
 
@@ -51,44 +61,109 @@
             }
 
             return o;
-        }, isArray:function (a) {
+        }
+        ,getObject: function(s) {
+            return alias[s];
+        }
+        ,isArray: function (a) {
             return (a instanceof Array);
-        }, isFunction:function (f) { //todo: need to modify this checking
+        }
+        ,isFunction: function (f) { //todo: need to modify this checking
             return (f instanceof Function); //(f.apply && f.call);
-        }, isNumber:function (n) {
+        }
+        ,isNumber: function (n) {
             return (typeof n == 'string' || typeof n == 'number') && !isNaN(n - 0) && n !== '';
-        }, isString:function (s) {
+        }
+        ,isString: function (s) {
             return (typeof(s) === 'string');
-        }, isObject:function (o) {
+        }
+        ,isObject: function (o) {
             return (o instanceof Object) && !this.isArray(o) && !this.isFunction(o);
-        }, ns:function (s) {
+        }
+        /**
+         @ns    leya.ns
+         @param {String} Namespace
+         @param {Boolean} ExistenceOnly
+         @return Object or Undefined/false
+        */
+        ,ns: function (s, is) {
             if (this.isString(s)) {
                 var a = s.split('.'),
                     o = win;
 
-                this.each(a, function (v) {
-                    o = o[v] = o[v] || {};
-                });
-                return o;
+                if(is) {
+                    this.each(a, function (v) {
+                        if(!o[v]) {
+                            o = o[v];
+                            return false;
+                        } else {
+                            o = o[v];
+                        }
+                    });
+                    return o;
+                } else {
+                    this.each(a, function (v) {
+                        o = o[v] = o[v] || {};
+                    });
+                    return o;
+                }
             }
             return false;
         }
+        /**
+         @ns    leya.ref
+         @param {String} Namespace
+         @param {String} Name
+         @param {Object} Object to reference
+         @return False if error
+         */
+        ,ref: function(ns, s, o) {
+            var ns = this.ns(ns,true);
+
+            if(!ns) {
+                return false;
+            }
+            ns.__proto__[s] = o;
+        }
+        /**
+         @ns    leya.reg
+         @param {String} Alias
+         @param {Object} Object to add
+         @return False if existing
+         */
+        ,reg: function(s, o) {
+            if(alias[s]) {
+                return false;
+            }
+            alias[s] = o;
+        }
     };
+
+    //leya.ref('leya.isObject', leya.ns);
+    leya.isObject = leya.ns;
+
+    win.leya = leya;
+
+    // @ns: leya.Panel
+    leya.create('leya.Model', {
+        alias: 'model'
+        ,init: function() {
+
+        }
+    });
 
     // @ns: leya.Panel
     leya.create('leya.Panel', {
+        alias: 'panel'
+        ,extend: 'leya.Model'
+        ,init: function() {
+
+        }
+    });
+
+    // @ns: leya.Form
+    leya.create('leya.Form', {
 
     });
-    /*
-     // @ns: leya.Form
-     leya.Form = function(c) {
 
-     };
-
-     // @ns: leya.FormField
-     leya.FormField = function(c) {
-
-     };*/
-
-    win.leya = leya;
 })();
